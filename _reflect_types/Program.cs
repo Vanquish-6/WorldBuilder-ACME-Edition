92fafffc74d34e1aa42b00b5a13a1cae44207e1c
Vanquish-6 <179872957+Vanquish-6@users.noreply.github.com>
2026-04-09 15:05:42 -0700

    monster editor improvements

--- _reflect_types/Program.cs
+++ _reflect_types/Program.cs
@@ -1,7 +1,4 @@
 using DatReaderWriter.Enums;
-using DatReaderWriter.Types;
 
-foreach (var n in Enum.GetNames(typeof(VertexType)))
-    Console.WriteLine("VertexType." + n);
-foreach (var n in Enum.GetNames(typeof(CullMode)))
-    Console.WriteLine("CullMode." + n);
+foreach (var n in Enum.GetNames(typeof(StipplingType)))
+    Console.WriteLine($"StipplingType.{n}");
